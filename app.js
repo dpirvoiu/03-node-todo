9d9ad653db5a6e571745c2cfd11509938aa96857
dpirvoiu <adisor.pdw@gmail.com>
2023-05-20 22:42:05 +0300

    chore: update port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/api/v1/tasks", tasks); // this is the root path
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
