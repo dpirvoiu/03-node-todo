3bb2ea017ddd146e7cff6425eb932a12ce1a4565
dpirvoiu <adisor.pdw@gmail.com>
2023-05-20 15:52:37 +0300

    chore: progress first app

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,9 +2,29 @@
 // import them and callback them on routes
 
 const getAllTasks = (req, res) => {
-  res.send("all items from controllers");
+  res.send("Get all tasks");
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.send("Get single  task");
+};
+
+const updateTask = (req, res) => {
+  res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete  task");
 };
 
 module.exports = {
   getAllTasks,
+  createTask,
+  deleteTask,
+  updateTask,
+  getTask,
 };
